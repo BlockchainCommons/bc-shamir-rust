f0b62432e86f0756b20327e9b43e1be91a90214b
Wolf McNally <wolf@wolfmcnally.com>
2025-02-04 01:37:26 -0800

    Aligned to dependencies. Prep for release.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-shamir/0.4.1")]
+#![doc(html_root_url = "https://docs.rs/bc-shamir/0.5.0")]
 #![warn(rust_2018_idioms)]
 
 //! ## Introduction
@@ -16,7 +16,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-shamir = "0.4.1"
+//! bc-shamir = "0.5.0"
 //!```
 //!
 //! ## Usage
